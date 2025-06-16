c8dc71888d3a27c724342ae71654ebe950f45bb8
AaronPFDev <aaronpesqueirafarina@gmail.com>
2025-06-16 23:34:09 +0200

    cambiado puerto de arranque del servidor: 3000

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get("/", (req, res) => {
 });
 
 // Puerto
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
