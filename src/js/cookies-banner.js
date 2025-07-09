85b299533a23b0e4736dd3981d4927d469924824
AaronPFDev <aaronpesqueirafarina@gmail.com>
2025-07-09 11:08:47 +0200

    añadido banner de cookies, cookies-banner.js y politica-cookies.html

---
+++ src/js/cookies-banner.js
@@ -0,0 +1,22 @@
+// Banner de cookies para toda la web
+(function () {
+  if (localStorage.getItem("cookiesAccepted") === "true") return;
+
+  var banner = document.createElement("div");
+  banner.id = "cookies-banner";
+  banner.innerHTML = `
+    <div class="cookies-banner-content">
+      <span>
+        Utilizamos cookies para mejorar tu experiencia. Más información en nuestra
+        <a href="politica-cookies.html" class="cookies-link">Política de Cookies</a>.
+      </span>
+      <button id="accept-cookies-btn" class="accept-cookies-btn">Aceptar</button>
+    </div>
+  `;
+  document.body.appendChild(banner);
+
+  document.getElementById("accept-cookies-btn").onclick = function () {
+    localStorage.setItem("cookiesAccepted", "true");
+    banner.remove();
+  };
+})();
